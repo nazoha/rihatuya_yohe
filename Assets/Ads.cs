72fc12f0d8bed4b18f24aac72f44e4a324b01640
haratatsurou <hara.tatsurou@gmail.com>
2016-02-29 01:36:16 +0900

    Asset追加、広告を再生する機能を追加

---
+++ Assets/Ads.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour {
+
+	// Use this for initialization
+    void Awake()
+    {
+        Advertisement.Initialize("041851cf60884ee9bb5b98119bde0ea6d3555cbf43ecaf26a5a6da151fe1ca1c");
+    }
+    public void showAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+}
