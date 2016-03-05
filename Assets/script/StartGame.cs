dcf39d5fb8d9012d84e4c33e3474af67b7154a7f
haratatsurou <hara.tatsurou@gmail.com>
2016-03-05 17:57:57 +0900

    広告撤去、Startbuttonを押すとstartuiが消えるように

---
+++ Assets/script/StartGame.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.UI;
+
+//  startui/background/startbuttonにアタッチ
+public class StartGame : MonoBehaviour {
+
+	void Start () {
+        var tapStart = this.UpdateAsObservable()
+            .Where(startButton => Input.GetMouseButtonDown(0));
+        tapStart
+        .Subscribe(statButton =>
+        {
+
+        });
+	}
+   public void StartButton()
+    {
+        //UIの非表示
+        GameObject.Find("startui").GetComponent<Canvas>().enabled = false;
+        //ボタンが押されないようにする
+        this.GetComponent<Button>().interactable = false;
+        print("スタートゲーム");
+    }
+	
+
+}
