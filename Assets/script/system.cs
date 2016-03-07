ca914e8f5c6e0798ff4dbd542abef3d940b69682
haratatsurou <hara.tatsurou@gmail.com>
2016-03-07 18:19:32 +0900

    サンプルシーンの追加とはさみを上下する画像を追加

---
+++ Assets/script/system.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System.Collections;
+
+public class system : MonoBehaviour {
+    [Header("はさみの可動域")]
+    public float y=10f;
+    [Header("はさみの速さ")]
+    public float speed = 1f;
+    private GameObject scissors;
+    void Awake()
+    {
+        scissors = GameObject.Find("hasami");
+    }
+
+	void Start () {
+        var movescissors = this.UpdateAsObservable().Select(_=>_);
+        movescissors.Subscribe(_ => MoveScissors());
+	}
+    //はさみの上下に関するメソッド
+    void MoveScissors()
+    {
+     var   scissorspos = scissors.GetComponent<Transform>();
+        scissorspos.position = new Vector2(scissorspos.position.x, 2+Mathf.PingPong(Time.time*speed, y-2));
+    }
+}
+
