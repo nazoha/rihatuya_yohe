13b0ce0bcb234397ddb8ac9fc02799e7037b7850
haratatsurou <hara.tatsurou@gmail.com>
2016-03-24 16:05:16 +0900

    仮画像配置と上下する機能及びクリックしたら一定数右に行く機能を追加

---
+++ Assets/script/Scissors.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class Scissors : MonoBehaviour
+{
+
+    [Header("はさみの可動域")]
+    public float y = 10f;
+    [Header("はさみの速さ")]
+    public float speed = 1f;
+    private GameObject scissors;
+    private float limitpos = 3f;
+    IObservable<Unit> moverleft;
+    IObservable<Unit> rightmove;
+    float ypos;
+
+    void Awake()
+    {
+        scissors = GameObject.Find("hasami");
+        ypos = 2;
+    }
+    void Start()
+    {
+        //マウスの右移動
+        rightmove = this.UpdateAsObservable()
+             .FirstOrDefault(_ => Input.GetMouseButtonDown(0));
+        rightmove
+            .Subscribe(_ =>
+            {
+                Right();
+                print("終わり");
+                ypos= scissors.GetComponent<Transform>().position.y;
+            }
+            );
+        //マウスの上下移動 1クリックしたらupanddonwscissorsを終了させる
+        var move = this.UpdateAsObservable();
+        move
+            .TakeUntil(rightmove)
+            .Subscribe(_ => UpandDown());
+    }
+
+    //はさみの上下に関するメソッド
+    void UpandDown()
+    {
+     
+        timer += Time.deltaTime;
+        var scissorspos = scissors.GetComponent<Transform>();
+        scissorspos.position = new Vector2(scissorspos.position.x, 2 + Mathf.PingPong((timer* speed), y - 2));
+    }
+
+    //はさみを右に移動させるメソッド
+    float timer = 0;
+    bool movedirection = false;
+    void Right()
+    {
+        timer = 0;
+        var scissorspos = scissors.GetComponent<Transform>();
+        var moveright = this.UpdateAsObservable()
+
+        .TakeWhile(_=>!movedirection);
+        //.Where(x => (int)scissorspos.position.x < limitpos);
+
+        moveright
+            .Subscribe(move =>
+            {
+                    //時間ごとに更新
+                    timer += Time.deltaTime;
+                    //scissorspos.position = new Vector2(Mathf.PingPong(timer * speed,y-2), scissorspos.position.y);
+                    scissorspos.position = Vector2.Lerp(new Vector2(0, scissorspos.position.y), new Vector2(timer * speed, scissorspos.position.y), 1f);
+                    if ((int)scissorspos.position.x == limitpos)
+                    {
+                        movedirection = true;
+                        Left();
+                    }
+
+            }
+            );
+    }
+    int i = 0;
+
+    void Left()
+    {
+        var scissorspos = scissors.GetComponent<Transform>();
+        moverleft = this.UpdateAsObservable()
+            .TakeWhile(_ => movedirection); 
+        moverleft
+            .Subscribe(_ =>
+            {
+                    timer -= Time.deltaTime;
+                    //scissorspos.position = new Vector2(speed * timer, scissorspos.position.y);
+                   scissorspos.position = Vector2.Lerp(new Vector2(0, scissorspos.position.y), new Vector2(timer * speed, scissorspos.position.y), 1f);
+
+                    print("主家えじゃ");
+                    if (scissorspos.position.x<=0f)
+                    {
+                        movedirection = false;
+                        Invoke("Start", 0.3f);
+                        print("ｙ座標="+ypos);
+
+                    }
+                
+            });
+    }
+
+}
--- Assets/script/system.cs
+++ Assets/script/system.cs
@@ -3,26 +3,104 @@ using UniRx;
 using UniRx.Triggers;
 using System.Collections;
 
-public class system : MonoBehaviour {
+public class system : MonoBehaviour
+{
     [Header("はさみの可動域")]
-    public float y=10f;
+    public float y = 10f;
     [Header("はさみの速さ")]
     public float speed = 1f;
     private GameObject scissors;
+
+    private float limitpos = 3f;
+
+    IObservable<Unit> rightmovescissors;
     void Awake()
     {
         scissors = GameObject.Find("hasami");
     }
 
-	void Start () {
-        var movescissors = this.UpdateAsObservable().Select(_=>_);
-        movescissors.Subscribe(_ => MoveScissors());
-	}
-    //はさみの上下に関するメソッド
-    void MoveScissors()
+    void Start()
     {
-     var   scissorspos = scissors.GetComponent<Transform>();
-        scissorspos.position = new Vector2(scissorspos.position.x, 2+Mathf.PingPong(Time.time*speed, y-2));
+        //マウスの右移動
+        rightmovescissors = this.UpdateAsObservable()
+           .First(_ => Input.GetMouseButtonDown(0));
+        rightmovescissors
+            .Subscribe(_ =>
+            {
+                RightScissors();
+                print("終わり");
+            }
+
+        );
+        //マウスの上下移動 1クリックしたらupanddonwscissorsを終了させる
+        var movescissors = this.UpdateAsObservable();
+        movescissors
+            .TakeUntil(rightmovescissors)
+            .Subscribe(_ => UpandDownScissors());
+
+
+        var upmousebutton = this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonUp(0));
+    }
+
+    //はさみの上下に関するメソッド
+    void UpandDownScissors()
+    {
+        var scissorspos = scissors.GetComponent<Transform>();
+        scissorspos.position = new Vector2(scissorspos.position.x, 2 + Mathf.PingPong(Time.time * speed, y - 2));
+    }
+    private IObservable<Unit> moveright;
+    //はさみを右に移動させるメソッド
+    float timer = 0;
+    bool movedirection = false;
+    void RightScissors()
+    {
+        var scissorspos = scissors.GetComponent<Transform>();
+        moveright = this.UpdateAsObservable();
+            //.TakeUntil(rightmovescissors)
+            //.Where(x => (int)scissorspos.position.x < limitpos);
+
+        moveright
+            .Subscribe(move =>
+            {
+                if (!movedirection)
+                {
+                    //時間ごとに更新
+                    timer += Time.deltaTime;
+                    //scissorspos.position = new Vector2(Mathf.PingPong(timer * speed,y-2), scissorspos.position.y);
+                    scissorspos.position = Vector2.Lerp(new Vector2(0, scissorspos.position.y), new Vector2(timer * speed, scissorspos.position.y), 1f);
+                    print("aaaaa");
+                    if ((int)scissorspos.position.x == limitpos)
+                    {
+                        movedirection = true;
+                    }
+                }
+                else
+                {
+                    LeftScissors();
+                    print("ほげ");
+                }
+            }
+            );
+    }
+    private IObservable<Unit> moverleft;
+    void LeftScissors()
+    {
+        var scissorspos = scissors.GetComponent<Transform>();
+        moverleft = this.UpdateAsObservable();
+            //.TakeUntil(moveright)
+            //.Where(x => 0f < scissorspos.position.x);
+        moverleft
+            .Subscribe(_ =>
+            {
+                timer -= Time.deltaTime;
+                scissorspos.position = new Vector2(timer * speed, scissorspos.position.y);
+                print("主家えじゃ");
+                if(0f < scissorspos.position.x)
+                {
+                    movedirection = false;
+                }
+            });
     }
 }
 
